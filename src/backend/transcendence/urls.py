9b2b1e448632194a8ea62229147585af9a55de35
joao-per <rodriguespereirajoaopaulo@gmail.com>
2024-02-13 23:11:14 +0000

    Added basic server in python Django, configured Django admin dashboard, added users and then configured /home and /login pages. Also added basic database functions

---
+++ src/backend/transcendence/urls.py
@@ -0,0 +1,15 @@
+"""transcendence URL Configuration
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+"""
+from django.urls import path, include
+from django.contrib import admin  # Import the admin module
+from .views import home_view
+from .views import login_view
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home_view, name='home'),  # Adiciona a URL para a página inicial.
+    path('accounts/', include('accounts.urls')),  # Inclui as URLs da app 'accounts'.
+    path('login/', login_view, name='login'),
+]
