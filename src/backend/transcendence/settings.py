5a9f4037aa0990c7eebfb92f976a3acb60f56d7c
joao-per <rodriguespereirajoaopaulo@gmail.com>
2024-02-15 08:32:59 +0000

    Changed database from sqlite to PostgresSQL. Fixed some minor errors on register

--- src/backend/transcendence/settings.py
+++ src/backend/transcendence/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'transcendence.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'transcendence',
+        'USER': 'osparolos',
+        'PASSWORD': 'coxinha123',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
