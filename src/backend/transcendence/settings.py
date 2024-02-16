d501a143f15156a923ce63f53693fc202b0e2164
joao-per <rodriguespereirajoaopaulo@gmail.com>
2024-02-16 12:03:06 +0000

    Added some more information and Homem Do Leme song - abaiao-r request

--- src/backend/transcendence/settings.py
+++ src/backend/transcendence/settings.py
@@ -143,6 +143,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    '/static',
+    # other directories...
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
