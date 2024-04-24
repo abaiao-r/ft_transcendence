a886370c6ab6ffbc5d4967dd67d08abae705805e
“Gabriel <gguerrafranco@gmail.com>
2024-04-24 10:27:11 +0100

    Changed id of double pong player select button and added same effects as the regular select button. Both regular and double pong now start when clicking new buttons.

--- src/game/js/double-pong.js
+++ src/game/js/double-pong.js
@@ -1116,13 +1116,13 @@ async function main(){
 	renderer.setAnimationLoop(animate);
 }
 
-// document.addEventListener('DOMContentLoaded', function() {
-// 	const buttons = document.querySelectorAll('.play-menu-button');
-// 	const doubleButton = buttons[2];
-// 	doubleButton.addEventListener('click', startGame);
-// 	function startGame() {
-// 		cpu = [0, 1, 1, 1];
-// 		document.getElementById('double-pong').style.display = 'block';
-// 		main();
-// 	}
-// });
+document.addEventListener('DOMContentLoaded', function() {
+	const doubleButton = document.getElementById('start-double-match');
+	doubleButton.addEventListener('click', startGame);
+	function startGame() {
+		cpu = [0, 1, 1, 1];
+		document.getElementById('double-pong-match-options').style.display = 'none';
+		document.getElementById('double-pong').style.display = 'block';
+		main();
+	}
+});
