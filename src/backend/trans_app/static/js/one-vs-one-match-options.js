1d2d3ad229af514c4e573b2d164ce558bf182e03
“Gabriel <gguerrafranco@gmail.com>
2024-05-06 15:17:26 +0100

    Added buttons and logic for adding guest players in 1v1. Actual login of guest players is not implemented yet

--- src/backend/trans_app/static/js/one-vs-one-match-options.js
+++ src/backend/trans_app/static/js/one-vs-one-match-options.js
@@ -16,14 +16,18 @@ let playerStatesPong = {
     p2: "center"
 };
 
+let p2Selected;
+
 // Reset initial states for players
 function resetPlayerStatesPong(){
+    p2Selected = false;
     playerStatesPong = {
         p1: "center",
         p2: "center"
     };
     // Show center elements
-    toggleDisplay(["p1-center", "p2-center", "arrow-left-center-p1", "arrow-right-center-p1", "arrow-left-center-p2", "arrow-right-center-p2"], "block");
+    toggleDisplay(["p1-center", "arrow-left-center-p1", "arrow-right-center-p1"], "block");
+    toggleDisplay(["p2-center", "arrow-left-center-p2", "arrow-right-center-p2"], "none");
     // Make center arrows black
     document.getElementById("arrow-left-center-p1").style.color = "black";
     document.getElementById("arrow-right-center-p1").style.color = "black";
@@ -34,6 +38,8 @@ function resetPlayerStatesPong(){
     document.getElementById("player-choosed-right-side").style.border = "5px solid lightgray";
     // Hide side elements
     toggleDisplay(["p1-left-side", "p1-right-side", "p2-left-side", "p2-right-side", "arrow-right-left-side-p1", "arrow-left-right-side-p1", "arrow-right-left-side-p2", "arrow-left-right-side-p2"], "none");
+    // Show add guest button
+    toggleDisplay(["add-guest"], "block");
 };
 
 // Function to update background color and image based on player states
@@ -87,7 +93,7 @@ function transitionPlayerPong(player, state, hideIds, showIds) {
         toggleDisplay(showIds, "block");
         playerStatesPong[player] = state;
         updateBackgroundColorAndImagePong();
-        // call funtion updateArrowColorPong
+        // call function updateArrowColorPong
         updateArrowColorPong(player, state);
     }
     else{
@@ -98,17 +104,70 @@ function transitionPlayerPong(player, state, hideIds, showIds) {
 // Function to initialize event listeners for player transitions
 function initializeEventListenersPong(player) {
     document.getElementById(`arrow-left-center-${player}`).addEventListener("click", () => transitionPlayerPong(player, "left", [`${player}-center`, `arrow-left-center-${player}`, `arrow-right-center-${player}`], [`${player}-left-side`, `arrow-right-left-side-${player}`]));
-    document.getElementById(`arrow-right-center-${player}`).addEventListener("click", () =>  transitionPlayerPong(player, "right", [`${player}-center`, `arrow-left-center-${player}`, `arrow-right-center-${player}`], [`${player}-right-side`, `arrow-left-right-side-${player}`]));
-    document.getElementById(`arrow-right-left-side-${player}`).addEventListener("click", () =>  transitionPlayerPong(player, "center", [`${player}-left-side`, `arrow-right-left-side-${player}`], [`${player}-center`, `arrow-left-center-${player}`, `arrow-right-center-${player}`]));
-    document.getElementById(`arrow-left-right-side-${player}`).addEventListener("click", () =>  transitionPlayerPong(player, "center", [`${player}-right-side`, `arrow-left-right-side-${player}`], [`${player}-center`, `arrow-left-center-${player}`, `arrow-right-center-${player}`]));
-    document.getElementById(`player-choosed-left-side`).addEventListener("click", () =>  transitionPlayerPong(player, "left", [`${player}-center`, `arrow-left-center-${player}`, `arrow-right-center-${player}`], [`${player}-left-side`, `arrow-right-left-side-${player}`]));
-    document.getElementById(`player-choosed-right-side`).addEventListener("click", () =>  transitionPlayerPong(player, "right", [`${player}-center`, `arrow-left-center-${player}`, `arrow-right-center-${player}`], [`${player}-right-side`, `arrow-left-right-side-${player}`]));
-
+    document.getElementById(`arrow-right-center-${player}`).addEventListener("click", () => transitionPlayerPong(player, "right", [`${player}-center`, `arrow-left-center-${player}`, `arrow-right-center-${player}`], [`${player}-right-side`, `arrow-left-right-side-${player}`]));
+    document.getElementById(`arrow-right-left-side-${player}`).addEventListener("click", () => transitionPlayerPong(player, "center", [`${player}-left-side`, `arrow-right-left-side-${player}`], [`${player}-center`, `arrow-left-center-${player}`, `arrow-right-center-${player}`]));
+    document.getElementById(`arrow-left-right-side-${player}`).addEventListener("click", () => transitionPlayerPong(player, "center", [`${player}-right-side`, `arrow-left-right-side-${player}`], [`${player}-center`, `arrow-left-center-${player}`, `arrow-right-center-${player}`]));
+    document.getElementById(`player-choosed-left-side`).addEventListener("click", () => transitionPlayerPong(player, "left", [`${player}-center`, `arrow-left-center-${player}`, `arrow-right-center-${player}`], [`${player}-left-side`, `arrow-right-left-side-${player}`]));
+    document.getElementById(`player-choosed-right-side`).addEventListener("click", () => transitionPlayerPong(player, "right", [`${player}-center`, `arrow-left-center-${player}`, `arrow-right-center-${player}`], [`${player}-right-side`, `arrow-left-right-side-${player}`]));
 }
-
 
 //Initialize event listeners for all players
 for (let player of Object.keys(playerStatesPong)) {
     initializeEventListenersPong(player);
 }
 
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('add-guest').addEventListener('click', function(){
+        if (p2Selected)
+            return;
+        toggleDisplay(["login-guest-box", "login-guest"], "block");
+        toggleDisplay(["sign-up-guest"], "none");
+    });
+    document.getElementById('close-add-guest').addEventListener('click', function(){
+        toggleDisplay(["login-guest-box", "login-guest", "sign-up-guest"], "none");
+    });
+    document.getElementById('close-add-guest-signup').addEventListener('click', function(){
+        toggleDisplay(["login-guest-box", "login-guest", "sign-up-guest"], "none");
+    });
+    document.getElementById('sign-up-guest-button').addEventListener('click', function(){
+        toggleDisplay(["login-guest"], "none");
+        toggleDisplay(["sign-up-guest"], "block");
+    });
+    document.getElementById('login-guest-link').addEventListener('click', function(){
+        toggleDisplay(["sign-up-guest"], "none");
+        toggleDisplay(["login-guest"], "block");
+    });
+    // TEST BUTTONS, ADD LOGIC TO REAL LOGIN/SIGNUP BUTTONS ONCE DONE
+    document.getElementById('test-add-guest1').addEventListener('click', function(){
+        toggleDisplay(["p2-center", "arrow-left-center-p2", "arrow-right-center-p2"], "block");
+        p2Selected = true;
+        toggleDisplay(["add-guest"], "none");
+        toggleDisplay(["login-guest-box", "login-guest", "sign-up-guest"], "none");
+    });
+    document.getElementById('test-add-guest2').addEventListener('click', function(){
+        toggleDisplay(["p2-center", "arrow-left-center-p2", "arrow-right-center-p2"], "block");
+        p2Selected = true;
+        toggleDisplay(["add-guest"], "none");
+        toggleDisplay(["login-guest-box", "login-guest", "sign-up-guest"], "none");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const passwordInput = document.getElementById("login-guest-password");
+    const showPasswordButton = document.querySelector(".guest-show-password");
+    const hidePasswordButton = document.querySelector(".guest-hide-password");
+
+    hidePasswordButton.style.display = "none"; // Initially hide the "Hide Password" button
+
+    showPasswordButton.addEventListener("click", function() {
+        passwordInput.type = "text";
+        showPasswordButton.style.display = "none";
+        hidePasswordButton.style.display = "inline";
+    });
+
+    hidePasswordButton.addEventListener("click", function() {
+        passwordInput.type = "password";
+        showPasswordButton.style.display = "inline";
+        hidePasswordButton.style.display = "none";
+    });
+});
