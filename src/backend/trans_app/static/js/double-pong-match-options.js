666729a3f43ad36452d1e872cf4aecfe3cf06199
“Gabriel <gguerrafranco@gmail.com>
2024-05-07 10:05:28 +0100

    Remove player button is working in both modes. Also improved readability in files with a lot of elements with the same rules

--- src/backend/trans_app/static/js/double-pong-match-options.js
+++ src/backend/trans_app/static/js/double-pong-match-options.js
@@ -54,6 +54,8 @@ function resetPlayerStatesDouble(){
     toggleDisplay(["dp-arrow-right-left-side-p1", "dp-arrow-right-right-side-p1", "dp-arrow-left-left-side-p1", "dp-arrow-left-right-side-p1", "dp-arrow-right-far-left-side-p1", "dp-arrow-left-far-right-side-p1", "dp-arrow-right-left-side-p2", "dp-arrow-right-right-side-p2", "dp-arrow-left-left-side-p2", "dp-arrow-left-right-side-p2", "dp-arrow-right-far-left-side-p2", "dp-arrow-left-far-right-side-p2", "dp-arrow-right-left-side-p3", "dp-arrow-right-right-side-p3", "dp-arrow-left-left-side-p3", "dp-arrow-left-right-side-p3", "dp-arrow-right-far-left-side-p3", "dp-arrow-left-far-right-side-p3", "dp-arrow-right-left-side-p4", "dp-arrow-right-right-side-p4", "dp-arrow-left-left-side-p4", "dp-arrow-left-right-side-p4", "dp-arrow-right-far-left-side-p4", "dp-arrow-left-far-right-side-p4"], "none");
     // Show add guest button
     toggleDisplay(["dp-add-guest"], "block");
+	// Hide remove guest button
+	toggleDisplay(["dp-remove-guest"], "none");
 };
 
 // Function to update background color and image based on player states
@@ -105,9 +107,8 @@ function transitionPlayer(player, state, hideIds, showIds, previousState) {
         transitionPlayer(player, `far-${state}`, hideIds, [`dp-${player}-far-${state}-side`, `dp-arrow-${oppositeState}-far-${state}-side-${player}`], previousState);
     } else if ((state === "right" || state === "left") && previousState === `far-${state}`) {
         transitionPlayer(player, "center", hideIds, [`dp-${player}-center`, `dp-arrow-left-center-${player}`, `dp-arrow-right-center-${player}`], previousState);
-    } else {
+    } else
         return;
-    }
 }
 
 // Function to toggle display
@@ -133,26 +134,54 @@ for (let player of Object.keys(playerStates)) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+	// Shows the login prompt for guests
     document.getElementById('dp-add-guest').addEventListener('click', function(){
         if (p2Selected)
             return;
         toggleDisplay(["dp-login-guest-box", "dp-login-guest"], "block");
         toggleDisplay(["dp-sign-up-guest"], "none");
     });
+	// Hides the login and sign up prompts for guests
     document.getElementById('dp-close-add-guest').addEventListener('click', function(){
         toggleDisplay(["dp-login-guest-box", "dp-login-guest", "dp-sign-up-guest"], "none");
     });
+	// Hides the login and sign up prompts for guests
     document.getElementById('dp-close-add-guest-signup').addEventListener('click', function(){
         toggleDisplay(["dp-login-guest-box", "dp-login-guest", "dp-sign-up-guest"], "none");
     });
+	// Shows the sign up prompt for guests and hides the login prompt
     document.getElementById('dp-sign-up-guest-button').addEventListener('click', function(){
         toggleDisplay(["dp-login-guest"], "none");
         toggleDisplay(["dp-sign-up-guest"], "block");
     });
+	// Shows the login prompt for guests and hides the sign up prompt
     document.getElementById('dp-login-guest-link').addEventListener('click', function(){
         toggleDisplay(["dp-sign-up-guest"], "none");
         toggleDisplay(["dp-login-guest"], "block");
     });
+	// Removes player elements, if there is more than 1 player
+	document.getElementById('dp-remove-guest').addEventListener('click', function(){
+		if (playerCounter > 1) {
+			// Transition player from current position until reaching center
+			while (playerStates[`p${playerCounter}`] !== "center") {
+				if (playerStates[`p${playerCounter}`] === "left")
+					transitionPlayer(`p${playerCounter}`, "center", [`dp-p${playerCounter}-left-side`, `dp-arrow-left-left-side-p${playerCounter}`, `dp-arrow-right-left-side-p${playerCounter}`], [`dp-p${playerCounter}-center`, `dp-arrow-left-center-p${playerCounter}`, `dp-arrow-right-center-p${playerCounter}`], playerStates[`p${playerCounter}`]);
+				else if (playerStates[`p${playerCounter}`] === "right")
+					transitionPlayer(`p${playerCounter}`, "center", [`dp-p${playerCounter}-right-side`, `dp-arrow-left-right-side-p${playerCounter}`, `dp-arrow-right-right-side-p${playerCounter}`], [`dp-p${playerCounter}-center`, `dp-arrow-left-center-p${playerCounter}`, `dp-arrow-right-center-p${playerCounter}`], playerStates[`p${playerCounter}`]);
+				else if (playerStates[`p${playerCounter}`] === "far-left")
+					transitionPlayer(`p${playerCounter}`, "center", [`dp-p${playerCounter}-far-left-side`, `dp-arrow-right-far-left-side-p${playerCounter}`], [`dp-p${playerCounter}-center`, `dp-arrow-left-center-p${playerCounter}`, `dp-arrow-right-center-p${playerCounter}`], playerStates[`p${playerCounter}`]);
+				else if (playerStates[`p${playerCounter}`] === "far-right")
+					transitionPlayer(`p${playerCounter}`, "center", [`dp-p${playerCounter}-far-right-side`, `dp-arrow-left-far-right-side-p${playerCounter}`], [`dp-p${playerCounter}-center`, `dp-arrow-left-center-p${playerCounter}`, `dp-arrow-right-center-p${playerCounter}`], playerStates[`p${playerCounter}`]);
+			}
+			toggleDisplay([`dp-p${playerCounter}-center`, `dp-arrow-left-center-p${playerCounter}`, `dp-arrow-right-center-p${playerCounter}`], "none");
+			playerCounter--;
+			p4Selected = false;
+			if (playerCounter == 1)
+				toggleDisplay(["dp-remove-guest"], "none");
+			if (playerCounter == 3)
+				toggleDisplay(["dp-add-guest"], "block");
+		}
+	});
     // TEST BUTTONS, ADD LOGIC TO REAL LOGIN/SIGNUP BUTTONS ONCE DONE
     document.getElementById('dp-test-add-guest1').addEventListener('click', function(){
         playerCounter++;
@@ -160,8 +189,9 @@ document.addEventListener('DOMContentLoaded', function() {
         if (playerCounter == 4) {
             p4Selected = true;
             toggleDisplay(["dp-add-guest"], "none");
-            toggleDisplay(["dp-login-guest-box", "dp-login-guest", "dp-sign-up-guest"], "none");
         }
+		toggleDisplay(["dp-login-guest-box", "dp-login-guest", "dp-sign-up-guest"], "none");
+		toggleDisplay(["dp-remove-guest"], "block");
     });
     document.getElementById('dp-test-add-guest2').addEventListener('click', function(){
         playerCounter++;
@@ -169,8 +199,9 @@ document.addEventListener('DOMContentLoaded', function() {
         if (playerCounter == 4) {
             p4Selected = true;
             toggleDisplay(["dp-add-guest"], "none");
-            toggleDisplay(["dp-login-guest-box", "dp-login-guest", "dp-sign-up-guest"], "none");
         }
+		toggleDisplay(["dp-login-guest-box", "dp-login-guest", "dp-sign-up-guest"], "none");
+		toggleDisplay(["dp-remove-guest"], "block");
     });
 });
 
