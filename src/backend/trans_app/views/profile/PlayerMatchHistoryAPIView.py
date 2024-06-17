9be3df761af8963140d3b19cb543ea5544f43299
Andre Francisco Baiao Rolao Candido Da Silva <andrefranciscorolao@gmail.com>
2024-06-17 13:40:05 +0100

    fixer bug where normal game modes would failed the request for stats

--- src/backend/trans_app/views/profile/PlayerMatchHistoryAPIView.py
+++ src/backend/trans_app/views/profile/PlayerMatchHistoryAPIView.py
@@ -52,10 +52,14 @@ class PlayerMatchHistoryAPIView(APIView):
 
     def post(self, request):
         data = request.data
-        if data[1]["PlayerInfo"]["username"] != "":
-            user_check = data[1]["PlayerInfo"]["username"]
+        print("Data mofodasnonf: ", data)
+        if (data[0].get("Tournament") == "Yes"):
+            if data[1]["PlayerInfo"]["username"] != "":
+                user_check = data[1]["PlayerInfo"]["username"]
+            else:
+                user_check = data[1]["PlayerInfo"]["displayName"]
         else:
-            user_check = data[1]["PlayerInfo"]["displayName"]
+            user_check = data[1].get("Name")
         if (data[1].get("AI") == 1):
             temp = data[1]
             data[1] = data[2]
@@ -72,10 +76,14 @@ class PlayerMatchHistoryAPIView(APIView):
 
         def get_username_and_stats(data, index):
             if index < len(data):
-                if data[index]["PlayerInfo"]["username"] != "":
-                    username = data[index]["PlayerInfo"]["username"]
+                #if tournament, check if username is empty
+                if (data[0].get("Tournament") == "Yes"):
+                    if data[index]["PlayerInfo"]["username"] != "":
+                        username = data[index]["PlayerInfo"]["username"]
+                    else:
+                        username = data[index]["PlayerInfo"]["displayName"]
                 else:
-                    username = data[index]["PlayerInfo"]["displayName"]
+                    username = data[index].get("Name")
                 stats = [data[index].get("Side"), data[index].get("Score"), data[index].get("Bounces"), data[0].get("Match Time")]
             else:
                 username = None
